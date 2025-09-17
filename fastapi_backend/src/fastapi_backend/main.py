8bf52de3cfa155e5b35e682f13deb3fdd4961e8e
saqibmurtaza <saqibmurtazakhan@gmai.com>
2025-09-17 11:59:54 +0500

    Adjust the place of static folder

--- fastapi_backend/src/fastapi_backend/main.py
+++ fastapi_backend/src/fastapi_backend/main.py
@@ -11,6 +11,7 @@ from datetime import datetime, timezone, timedelta
 from .utils import get_service_duration, generate_time_slots
 from fastapi_backend.opening_hours import OPENING_HOURS
 from fastapi_backend.agents.config_agents import config
+from fastapi_backend.static import StaticFiles
 import logging
 
 # Setup logging
@@ -60,6 +61,8 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"], 
 )
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 # --------- ENDPOINTS---------
